79c7e701ac4d04e06d5ab911cf028bce2db39f14
Wooseok Lee <wl00ie19@gmail.com>
2024-03-27 18:27:19 +0900

    feat: 더보기 화면 기본 UI 구성(로그인 미연동)

--- ARI/ARI/Views/MoreInfo/MoreInfoView.swift
+++ ARI/ARI/Views/MoreInfo/MoreInfoView.swift
@@ -8,11 +8,112 @@
 import SwiftUI
 
 struct MoreInfoView: View {
+    @State private var isLogin: Bool = false
+    @State private var isShowingAlert: Bool = false
+    let emailAddress: String = "abcd@example.com"
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationStack {
+            ScrollView {
+                LazyVStack {
+                    ZStack(alignment: .leading) {
+                        RoundedRectangle(cornerSize: CGSize(width: 10, height: 10))
+                            .foregroundStyle(.background2)
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text("ARI ~ % userinfo")
+                                Text(isLogin ? "\(emailAddress)" : "로그아웃 상태입니다")
+                                
+                            }
+                            .foregroundStyle(.text)
+                            
+                            Spacer()
+                            
+                            if isLogin {
+                                Button {
+                                    isShowingAlert.toggle()
+                                } label: {
+                                    Text("Logout")
+                                        .font(.title3.bold())
+                                }
+                            } else {
+                                Button {
+                                    isLogin = true
+                                } label: {
+                                    Text("Login")
+                                        .font(.title3.bold())
+                                }
+                            }
+                        }
+                        .padding()
+                    }
+                    
+                    
+                    
+                    ZStack(alignment: .leading) {
+                        RoundedRectangle(cornerSize: CGSize(width: 10, height: 10))
+                            .foregroundStyle(.background2)
+                        VStack(alignment: .leading) {
+                            Text("ARI ~ % ari --version")
+                            Text("ARI")
+                            Text("(Apple developer Random Interview)")
+                                .font(.subheadline)
+                            Text("Version 0.0.9")
+                        }
+                        .foregroundStyle(.text)
+                        .padding()
+                    }
+                    
+                    NavigationLink {
+                        Text("개인정보처리방침 뷰 연결")
+                    } label: {
+                        ZStack(alignment: .leading) {
+                            RoundedRectangle(cornerSize: CGSize(width: 10, height: 10))
+                                .foregroundStyle(.background2)
+                            VStack(alignment: .leading) {
+                                Text("ARI ~ % ari --privacy-policy")
+                            }
+                            .padding()
+                        }
+                    }
+                    
+                    NavigationLink {
+                        Text("크레딧 및 연락처 뷰 연결")
+                    } label: {
+                        ZStack(alignment: .leading) {
+                            RoundedRectangle(cornerSize: CGSize(width: 10, height: 10))
+                                .foregroundStyle(.background2)
+                            VStack(alignment: .leading) {
+                                Text("ARI ~ % ari --developers")
+                            }
+                            .padding()
+                        }
+                    }
+                    
+                }
+            }
+            .padding(EdgeInsets(top: 15, leading: 10, bottom: 15, trailing: 10))
+            .alert("로그아웃", isPresented: $isShowingAlert) {
+                Button("로그아웃", role: .destructive) {
+                    isLogin = false
+                }
+                
+            } message: {
+                Text("로그아웃 하시겠습니까?")
+            }
+            .fontDesign(.monospaced)
+            .background(.backGround)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Text("더 보기")
+                        .fontWeight(.bold)
+                }
+            }
+        }
     }
 }
 
 #Preview {
     MoreInfoView()
+        .preferredColorScheme(.dark)
 }
