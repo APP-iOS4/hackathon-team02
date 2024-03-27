0a068760d0fa2c0d7ab438eaa3fceb9ea41da872
Healthy <kangsworkspace@gmail.com>
2024-03-27 20:48:24 +0900

    feature: 로그인, 로그아웃 기능 추가

--- ARI/ARI/DataControl/DataTestView.swift
+++ ARI/ARI/DataControl/DataTestView.swift
@@ -9,23 +9,50 @@ import SwiftUI
 
 struct DataTestView: View {
     @EnvironmentObject private var questionModel: QuestionViewModel
+    @EnvironmentObject private var loginViewModel: LoginViewModel
     
     @State var question: String = ""
+    @State var isLogin: Bool = false
     
     var body: some View {
-        VStack {
+        VStack(spacing: 30) {
             Text(question)
+            
+            Text("\(isLogin)")
             
             Button {
                 question = questionModel.getRandomQuestion().question
             } label: {
                 Text("문제 가져오기 버튼")
             }
+            
+            Button {
+                loginViewModel.loginGoogle()
+            } label: {
+                Text("로그인 버튼")
+            }
+            
+            Button {
+                loginViewModel.logoutGoogle()
+            } label: {
+                Text("로그아웃 버튼")
+            }
+            
+            Button {
+                isLogin = checkIsUserLogin()
+            } label: {
+                Text("로그인 확인 버튼")
+            }
         }
+    }
+    
+    func checkIsUserLogin() -> Bool {
+        return loginViewModel.isUserLogin()
     }
 }
 
 #Preview {
     DataTestView()
         .environmentObject(QuestionViewModel())
+        .environmentObject(LoginViewModel())
 }
--- ARI/ARI/New Group/ARIApp.swift
+++ ARI/ARI/New Group/ARIApp.swift
@@ -18,17 +18,21 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 
 @main
 struct ARIApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     // 문제 데이터 뷰 모델
     @StateObject private var questionViewModel = QuestionViewModel()
     
-    // register app delegate for Firebase setup
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    // 로그인 데이터 뷰 모델
+    @StateObject private var loginViewModel = LoginViewModel()
     
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .preferredColorScheme(.dark)
                 .environmentObject(questionViewModel)
+                .environmentObject(loginViewModel)
         }
     }
 }
